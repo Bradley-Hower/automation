b340e871c37d0f6b1201438c8984687fb6109804
Bradley Hower <bradleyhower@proton.me>
2024-02-04 17:18:42 -0800

    complete

--- README.md
+++ README.md
@@ -4,5 +4,24 @@
 
 ## Author: Bradley Hower
 
+By automating tasks, greater efficiency can be achieved. Here there are five automation created.
+
+To input directories/folders, the directory is entered in reference to the current directory of the run. For example, if ran from downloads, `assets/user-docs` would specify a the subfolder "user-docs" within the assets folder.
+
+1. Create a folder - Creates a folders as named.
+2. Moved User Files - Moves files of a deleted user to a new temp folder. Old folder is eliminated.
+3. Document Sort - Sorts log and email documents to self-described folders.
+4. Log and Warning Parse - Parses a log file into two files, warnings, and errors.
+5. Backup - Creates a backup of a provided folder. Creates a new folder as described, copying new files thereto.
+
 ## Run
 
+To run the application, `python3 automation.py`
+
+Requires the following installtion via `pip install <name>`:
+
+- `Console`
+- `Prompt`
+- `os`
+- `shutil`
+- `re`
--- automation.py
+++ automation.py
@@ -1,11 +1,11 @@
 from rich.console import Console
 from rich.prompt import Prompt
-from rich.table import Table
 import os
 import shutil
 import re
 
 def create_folder(new_folder):
+  """Creates a folder as titled dictated by 'new_folder'"""
   try:
     os.mkdir(new_folder)
     console.print(f"A new folder named {new_folder} created")
@@ -13,6 +13,7 @@ def create_folder(new_folder):
     console.print(f"[bold]That folder exits or can't be created[/bold]")
 
 def move_user(user):
+  """Moves user documents from a folder into a temp folder. Removes prior folder."""
   try:
     temp_name = "temp_" + user
     source_path = f"assets/user-docs/{user}"
@@ -23,26 +24,78 @@ def move_user(user):
     console.print(f"[bold]That folder exits or can't be created[/bold]")
 
 def folder_sort(folder_to_sort):
+  """Sorts folders according to their extension, either '.log' or '.email'."""
+  files = os.listdir(folder_to_sort)
   try:
-    files = os.listdir(folder_to_sort)
-    log_matches = [file for file in files if re.search("log", file)]
-    subfolder = os.path.join(folder_to_sort, "logs")
+    log_matches = [file for file in files if re.search(".log", file)]
+    log_subfolder = os.path.join(folder_to_sort, "logs")
+    if not os.path.exists(log_subfolder):
+      os.makedirs(log_subfolder)
     for file in log_matches:
       source_path = os.path.join(folder_to_sort, file)
-      target_path = os.path.join(subfolder, file)
-      console.print(source_path)
-      console.print(target_path)
-      shutil.copy2(source_path, target_path)
+      target_path = os.path.join(log_subfolder, file)
+      shutil.move(source_path, target_path)
   except FileExistsError:
     console.print(f"[bold]That folder exits or can't be created[/bold]")
+  try:
+    email_matches = [file for file in files if re.search(".mail", file)]
+    email_subfolder = os.path.join(folder_to_sort, "email")
+    if not os.path.exists(email_subfolder):
+      os.makedirs(email_subfolder)
+    for file in email_matches:
+      source_path = os.path.join(folder_to_sort, file)
+      target_path = os.path.join(email_subfolder, file)
+      shutil.move(source_path, target_path)
+  except FileExistsError:
+    console.print(f"[bold]That folder exits or can't be created[/bold]")
+
+def log_parse(folder_to_parse):
+  """Parses log file contents according to either tags 'ERROR' or 'WARNING' into their respective separate log files."""
+  files = os.listdir(folder_to_parse)
+  for file in files:
+    source_path = os.path.join(folder_to_parse, file)
+    try:
+      with open(source_path, 'r') as rfile:
+        content = rfile.read()
+        contentlist = []
+        for line in re.findall(r".*ERROR.*", content):
+          contentlist.append(line + "\n")
+          merged = "".join(contentlist)
+          target_path = os.path.join(folder_to_parse, "errors.log.txt")
+          with open(target_path, "w") as writefile:
+            writefile.write(merged)
+    except FileNotFoundError:
+      raise FileNotFoundError
+    try:
+      with open(source_path, 'r') as rfile:
+        content = rfile.read()
+        contentlist = []
+        for line in re.findall(r".*WARNING.*", content):
+          contentlist.append(line + "\n")
+          merged = "".join(contentlist)
+          target_path = os.path.join(folder_to_parse, "warnings.log.txt")
+          with open(target_path, "w") as writefile:
+            writefile.write(merged)
+    except FileNotFoundError:
+      raise FileNotFoundError
+    
+def backup_script(folder_to_backup, backup_target):
+  """Creates backup copy of contents of a provided folder. Creates backup folder according to a name provided, with copies of files copied thereto."""
+  console.print(folder_to_backup)
+  console.print(backup_target)
+  try:
+    shutil.copytree(folder_to_backup, backup_target)
+  except FileNotFoundError:
+    raise FileNotFoundError
+
 
 console = Console()
 
 if __name__ == "__main__":
   while True:
     #Listed and colored blue
-    console.print("[blue]\n1. Create a Folder\n2. Move User Files\n3. Document Sort\n4. Something else\n5. Exit[/blue]")
-    choice = Prompt.ask("Choose a task (Enter the number)", choices=['1', '2', '3', '4'], default='5')
+    console.print("[blue]\n1. Create a Folder\n2. Move User Files\n3. Document Sort\n4. Log and Warning Parse\n5. Backup\n6. Exit[/blue]")
+    choice = Prompt.ask("Choose a task (Enter the number)", choices=['1', '2', '3', '4', '5', '6'], default='6')
     if choice == '1':
       new_folder = Prompt.ask("Enter the name of the folder to create")
       create_folder(new_folder)
@@ -53,6 +106,11 @@ if __name__ == "__main__":
       folder_to_sort = Prompt.ask("Enter the folder to sort")
       folder_sort(folder_to_sort)
     elif choice == '4':
-      pass
+      folder_to_parse = Prompt.ask("Enter the folder to parse logs")
+      log_parse(folder_to_parse)
+    elif choice == '5':
+      folder_to_backup = Prompt.ask("Enter the folder to backup (contents)")
+      backup_target = Prompt.ask("Enter the directory to backup to (folder will be created)")
+      backup_script(folder_to_backup, backup_target)
     else:
       break
